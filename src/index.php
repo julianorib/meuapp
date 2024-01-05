2192eeb8af49eb8706df3990a286e155773778c7
Juliano Ribeiro <julianorib@gmail.com>
2024-01-05 12:07:31 -0300

    meuapp

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu App</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <h1>Meu App</h1>
+        <br><br>
+        <?php
+        echo gethostname();
+        ?>
+    </div>
+</body>
+</html>
