fac8926112d881f454ffd91e47dedb8a24227d1b
Juliano Ribeiro <julianorib@gmail.com>
2024-01-05 14:36:28 -0300

    Corrigindo versão

--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Meu App 2</title>
+    <title>Meu App v1.0</title>
 </head>
 <body>
     <div style="text-align: center;">
-        <h1>Meu App 2</h1>
+        <h1>Meu App v1.0</h1>
         <br><br>
         <?php
         echo gethostname();
